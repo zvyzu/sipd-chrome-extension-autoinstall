33aca1621ef63dbe0e05c5d8aff5b105346480c6
evanvyz <zvyz@outlook.com>
2022-12-08 08:40:01 +0700

    Pengecekan git terinstall v2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 # Lokasi git terinstall
 exist64 = os.path.isfile('C:\Program Files\Git\cmd\git.exe')
@@ -7,6 +8,11 @@ existlocal = os.path.isfile('AppData\Local\Programs\Git\cmd\git.exe')
 
 # Mengecek git sudah terinstall
 if exist64 or existx86 or existlocal:
-    print('Git sudah di install')
+    print('Mengecek git sudah terinstall')
 else:
-    os.system('winget install --id Git.Git -e --source winget')
+    try: # Pengecekan terakhir jika lokasi git terinstall tidak diketahui
+        subprocess.call(['git'])
+        print('Git sudah terinstall')
+    except:
+        print('Mendownload dan Menginstall git')
+        os.system('winget install --id Git.Git -e --source winget')
