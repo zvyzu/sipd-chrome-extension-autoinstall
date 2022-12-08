d40a380720413502bce32513aded1982d8c0684d
evanvyz <zvyz@outlook.com>
2022-12-08 13:26:10 +0700

    Pengecekan git sudah terinstall v3 & menambahkan backup download

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import os
-import subprocess
-
-# Lokasi git terinstall
-exist64 = os.path.isfile('C:\Program Files\Git\cmd\git.exe')
-existx86 = os.path.isfile('C:\Program Files (x86)\git\Git\cmd\git.exe')
-existlocal = os.path.isfile('AppData\Local\Programs\Git\cmd\git.exe')
+from wget import download
+from subprocess import call, Popen, DEVNULL, STDOUT
 
 # Mengecek git sudah terinstall
-if exist64 or existx86 or existlocal:
-    print('Mengecek git sudah terinstall')
-else:
-    try: # Pengecekan terakhir jika lokasi git terinstall tidak diketahui
-        subprocess.call(['git'])
-        print('Git sudah terinstall')
+try:
+    call(['git'], stdout=DEVNULL, stderr=STDOUT)
+    print('Git sudah terinstall')
+except:
+    print('Mendownload & menginstall git')
+    try:
+        Popen('winget install --id Git.Git -e --source winget')
+        print('Git berhasil terinstall')
     except:
-        print('Mendownload dan Menginstall git')
-        os.system('winget install --id Git.Git -e --source winget')
+        print('Mendownload git')
+        download('https://github.com/git-for-windows/git/releases/download/v2.38.1.windows.1/Git-2.38.1-32-bit.exe')
+        print('Sedang menginstall git')
+        os.system('Git-2.38.1-32-bit.exe /VERYSILENT')
+        print('Git berhasil terinstall')
