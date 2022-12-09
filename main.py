dd171cb231dfc6d2f353a810f0c2b67da943ac76
evanvyz <zvyz@outlook.com>
2022-12-09 14:58:11 +0700

    merombak pengecekan, menambah git clone extension dan mendownload config.js

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 from wget import download
 from subprocess import call, Popen, DEVNULL, STDOUT
 
@@ -8,20 +9,39 @@ cx86 = os.path.isfile('C:\Program Files (x86)\Google\Chrome\Application\chrome.e
 win764 = os.path.isfile('C:\Program Files\Google\Application\chrome.exe')
 win7x86 = os.path.isfile('C:\Program Files (x86)\Google\Application\chrome.exe')
 
-try: # Mengecek git sudah terinstall
-    call(['git'], stdout=DEVNULL, stderr=STDOUT)
-    print('Git sudah terinstall')
-except:
-    print('Mendownload & menginstall git')
+def cek(): # Mengecek git sudah terinstall
     try:
-        Popen('winget install --id Git.Git -e --source winget')
-        print('Git berhasil terinstall.')
-    except:
+        call(['git'], stdout=DEVNULL, stderr=STDOUT)
+        return False
+    except OSError:
+        return True
+
+if cek(): # Mengecek git sudah terinstall
+    try:
+        Popen('wwinget install --id Git.Git -e --source winget')
+    except OSError as e:
         print('Mendownload git')
         download('https://github.com/git-for-windows/git/releases/download/v2.38.1.windows.1/Git-2.38.1-32-bit.exe')
         print('Sedang menginstall git')
         os.system('Git-2.38.1-32-bit.exe /VERYSILENT')
-        print('Git berhasil terinstall.')
+else:
+    print('Git sudah terinstall')
+
+while cek(): # Pengecekan sedang mendownload & menginstall git
+    if cek() == False:
+        break
+    print('Sedang mendownload & menginstall git |')
+    sleep(0.25)
+    print('Sedang mendownload & menginstall git /')
+    sleep(0.25)
+    print('Sedang mendownload & menginstall git -')
+    sleep(0.25)
+    print('Sedang mendownload & menginstall git \\')
+    sleep(0.25)
+else:
+    print('Sedang clone extension')
+    os.system('git clone https://github.com/agusnurwanto/sipd-chrome-extension.git D:\sipd-chrome-extension')
+    download('https://raw.githubusercontent.com/evanvyz/sipd-chrome-extension-autoinstall/main/config.js', out = 'D:\sipd-chrome-extension')
 
 if c64 or cx86 or win764 or win7x86: # Mengecek Google Chrome terinstall
     pass
