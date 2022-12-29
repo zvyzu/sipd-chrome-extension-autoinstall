860fa1034a903f88d9aa494ef219c52de33cb243
evanvyz <zvyz@outlook.com>
2022-12-29 10:12:35 +0700

    Menambahkan Menu

--- autoinstall.bat
+++ autoinstall.bat
@@ -14,11 +14,27 @@ exit
 :: Mengecek git terinstall
 ::------------------------
 git -v
-if %errorlevel% NEQ 0 ( cls ) else ( goto gitinstalled )
+if %errorlevel% NEQ 0 ( goto cls ) else ( goto gitinstalled )
+
+::----------------
+:: Mendownload Git
+::----------------
+:: url / link download git ( Silakan ganti jika ada git versi terbaru )
+:reinstallgit
+set urlgit=https://github.com/git-for-windows/git/releases/download/v2.39.0.windows.2/Git-2.39.0.2-32-bit.exe
+set filename=Git-2.39.0.2-32-bit.exe
+
+:: Download Git
+echo Mendownload git.....
+curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "%urlgit%" -o %filename% --ssl-no-revoke
+if %errorlevel% NEQ 0 ( cls ) else ( goto reqadmin )
+echo Mendownload git.....
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%urlgit%', '%filename%')"
 
 ::----------------------------
 :: Meminta akses administrator
 ::----------------------------
+:reqadmin
 IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
 >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
 ) ELSE (
@@ -47,20 +63,6 @@ if '%errorlevel%' NEQ '0' (
 ::---------------------------
 ::for /F "delims=" %%G in ('FORFILES /P "C:\Program Files (x86)\SMADAV" /M *.exe /S') do ( taskkill /F /IM %%G /T )
 ::cls
-
-::----------------
-:: Mendownload Git
-::----------------
-:: url / link download git ( Silakan ganti jika ada git versi terbaru )
-set urlgit=https://github.com/git-for-windows/git/releases/download/v2.39.0.windows.2/Git-2.39.0.2-32-bit.exe
-set filename=Git-2.39.0.2-32-bit.exe
-
-:: Download Git
-echo Mendownload git.....
-curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "%urlgit%" -o %filename% --ssl-no-revoke
-if %errorlevel% NEQ 0 ( cls ) else ( goto gitinstall )
-echo Mendownload git.....
-powershell -Command "(New-Object Net.WebClient).DownloadFile('%urlgit%', '%filename%')"
 
 ::----------------
 :: Menginstall Git
@@ -95,23 +97,103 @@ if exist "%driveloc%\sipd-chrome-extension\" (
 if not exist "%driveloc%\sipd-chrome-extension\.git\" ( rmdir /S /Q %driveloc%\sipd-chrome-extension\ )
 )
 
-:: Pengecekan
-if not exist "%driveloc%\sipd-chrome-extension\" ( goto gitclone )
-if exist "%driveloc%\sipd-chrome-extension\config.js.example" ( goto gitclone )
-powershell -Command "(New-Object Net.WebClient).DownloadFile('%urlexample%', '%driveloc%\sipd-chrome-extension\config.js.example')"
+:: Pengecekan file
+if exist "%driveloc%\sipd-chrome-extension\" (
+if not exist "%driveloc%\sipd-chrome-extension\manifest.json" ( rmdir /S /Q %driveloc%\sipd-chrome-extension\ ))
+if exist "%driveloc%\sipd-chrome-extension\" (
+if not exist "%driveloc%\sipd-chrome-extension\config.js.example" (
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%urlexample%', '%driveloc%\sipd-chrome-extension\config.js.example')"))
 
 :gitclone
 if exist "%driveloc%\sipd-chrome-extension\manifest.json" ( goto gitpull )
+:reclone
+if exist "%driveloc%\sipd-chrome-extension\" ( rmdir /S /Q %driveloc%\sipd-chrome-extension\ )
 git clone https://github.com/agusnurwanto/sipd-chrome-extension.git %driveloc%\sipd-chrome-extension
 goto configjs
 
 :gitpull
 git -C %driveloc%\sipd-chrome-extension\ pull origin master
-if not exist "%driveloc%\sipd-chrome-extension\config.js" ( goto configjs ) else ( goto done )
 
 :configjs
+if exist "%driveloc%\sipd-chrome-extension\config.js" ( goto menu )
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%urljs%', '%driveloc%\sipd-chrome-extension\config.js')"
-goto done
+goto menu
+
+::----------------------------
+:: Cek / install Google Chrome
+::----------------------------
+:chromecheck
+if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" ( goto opensipd )
+if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" ( goto opensipd )
+if exist "%LocalAppData%\Google\Chrome\Application\chrome.exe" ( goto opensipd )
+if exist "C:\Program Files\Google\Application\chrome.exe" ( goto opensipd )
+if exist "C:\Program Files (x86)\Google\Application\chrome.exe" ( goto opensipd )
+if exist "%LocalAppData%\Google\Chrome\Application\chrome.exe" ( goto opensipd )
+
+:chromeconfirm
+cls
+echo:
+echo Download dan install Google Chrome?
+echo:
+set /p chromeinstall=Ketik "y" lalu Enter jika iya, Ketik "n" lalu Enter jika tidak: 
+if %chromeinstall% == y ( goto chromedownload ) else ( goto lsmenu)
+
+:chromedownload
+set urlchrome=https://s.id/urlchrome
+set chromesetup=ChromeSetup.exe
+cls
+echo Mendownload Chrome.....
+curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "%urlchrome%" -o %chromesetup% --ssl-no-revoke
+if %errorlevel% NEQ 0 ( cls ) else ( goto chromeinstall )
+echo Mendownload Chrome.....
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%urlchrome%', '%chromesetup%')"
+
+:chromeinstall
+cls
+echo Menginstall Chrome.....
+%chromesetup%
+exit
+
+:opensipd
+cls
+echo:
+echo Buka SIPD?
+echo:
+set /p openchrome=Ketik "y" lalu Enter jika iya, Ketik "n" lalu Enter jika tidak: 
+if %openchrome% == y (
+start chrome https://madiunkab.sipd.kemendagri.go.id/daerah
+exit
+) else ( goto lsmenu )
+
+:: -----------------
+:: Menu Auto Install
+:: -----------------
+:menu
+if exist "%filename%" ( del /f %filename% )
+if exist "%chromesetup%" ( del /f %chromesetup% )
+timeout 5
+:lsmenu
+cls
+echo Menu Auto Install sipd-chrome-extension:
+echo 1 Buka SIPD.
+echo 2 Update ulang aplikasi sipd-chrome-extension.
+echo 3 Clone ulang aplikasi sipd-chrome-extension.
+echo 4 Install ulang aplikasi Git.
+echo 5 Download dan install Google Chrome
+echo 6 Keluar.
+echo:
+set /p menu=Pilih 1-6 lalu Enter: 
+if %menu% == 1 ( goto chromecheck )
+if %menu% == 2 ( goto gitpull )
+if %menu% == 3 ( goto reclone )
+if %menu% == 4 ( goto reinstallgit )
+if %menu% == 5 ( goto chromeconfirm )
+if %menu% == 6 ( exit )
+if %menu% == 0 ( exit )
+if %menu% GTR 6 (
+cls
+goto lsmenu) else ( exit )
+exit
 
 ::-----------
 :: Finalisasi
@@ -119,8 +201,6 @@ goto done
 :relaunch
 echo Jalankan aplikasi ini lagi.
 if exist "%filename%" ( del /f %filename% )
+if exist "%chromesetup%" ( del /f %chromesetup% )
 pause
 exit
-:done
-if exist "%filename%" ( del /f %filename% )
-timeout 5
